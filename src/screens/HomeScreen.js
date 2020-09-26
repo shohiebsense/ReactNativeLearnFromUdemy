23d1935a8e28c920f3ae5b1347ef449f8591ecc0
shohiebsense <shohieb.ahmad@gmail.com>
2020-09-26 20:08:01 +0700

    Added NavigationLesson

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import { View, Button, Text, StyleSheet } from 'react-native'
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There!!!</Text>
+const HomeScreen = (props) => {
+  const navigate = (navigationName) => {
+    props.navigation.navigate(navigationName)
+  }
+
+  return (
+    <View>
+      <Text style={styles.text}>Hi There!!!</Text>
+      <Button
+        onPress={() => {
+          console.log('Button presed')
+          navigate('Components')
+        }}
+        title="Go to components demo"
+      />
+      <TouchableOpacity
+        onPress={() => {
+          console.log('List Pressed')
+          navigate('List')
+        }}
+      >
+        <Text>Go To List Component</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
